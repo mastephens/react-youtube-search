d993e80e0ed39807896c2a89ae5148bac6e49b9f
Mike Stephens <mastephens@gmail.com>
2017-05-25 09:25:05 -0500

    update key

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
 
-const API_KEY = 'AIzaSyDmgABfM4bWzvpRixhNM5otkNLhH_EIHMc';
+const API_KEY = '';
 
 class App extends Component {
     constructor(props) {
